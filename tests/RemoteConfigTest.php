a84d7e484b26043e51466178b2a5fca9ec3a66c8
Kaique Garcia Menezes <kaique.menezes@linx.com.br>
2021-10-29 18:15:26 -0300

    Error logging tests

--- tests/RemoteConfigTest.php
+++ tests/RemoteConfigTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Log;
 use Linx\RemoteConfigClient\RemoteConfig;
 use GuzzleHttp\Client;
 use Symfony\Component\Cache\Simple\FilesystemCache;
@@ -54,6 +57,65 @@ class RemoteConfigTest extends PHPUnit_Framework_TestCase
             ->once()
             ->andReturn(true);
 
+        $remoteConfig->getClientConfig('client');
+    }
+
+    public function testLogWhenFails()
+    {
+        $remoteConfig = new RemoteConfig([
+            'host' => 'http://remote-config',
+            'username' => 'username@email.com',
+            'password' => 'password',
+            'application' => 'application2',
+            'environment' => 'environment',
+        ]);
+
+        $cacheMock = Mockery::mock(FilesystemCache::class);
+        $httpClientMock = Mockery::mock(Client::class);
+
+        $remoteConfig->setHttpClient($httpClientMock);
+        $remoteConfig->setCache($cacheMock);
+        $remoteConfig->setLoggerClass(Log::class);
+
+        $cacheMock
+            ->shouldReceive('has')
+            ->with(md5('/api/v1/configs/application2/client/environment'))
+            ->once()
+            ->andThrow(Exception::class, 'Testing Exception');
+
+        Log::shouldReceive('error')
+            ->withArgs(['Could not open cache from Redis', [
+                'client' => 'client',
+                'error_message' => 'Testing Exception',
+                'uri' => '/api/v1/configs/application2/client/environment',
+            ]])
+            ->once()
+            ->getMock()
+            ->shouldReceive('error')
+            ->withArgs(['Could not get data from Remote Config API', [
+                'current_cache' => null,
+                'error_message' => 'Testing ConnectException',
+                'path' => '/api/v1/configs/application2/client/environment',
+                'timeout' => 3,
+            ]])
+            ->once();
+
+        $httpClientMock
+            ->shouldReceive('request')
+            ->with('GET', 'http://remote-config/api/v1/configs/application2/client/environment', [
+                'auth' => ['username@email.com', 'password'],
+                'connect_timeout' => RemoteConfig::REQUEST_TIMEOUT,
+                'read_timeout' => RemoteConfig::REQUEST_TIMEOUT,
+                'timeout' => RemoteConfig::REQUEST_TIMEOUT,
+            ])
+            ->once()
+            ->andThrow(
+                ConnectException::class,
+                'Testing ConnectException',
+                new Request('http://remote-config', '/api/v1/configs/application2/client/environment')
+            );
+
+        self::expectException(ConnectException::class);
         $remoteConfig->getClientConfig('client');
     }
 }
